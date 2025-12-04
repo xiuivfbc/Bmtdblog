65887f667b0da62860f0dad0249d9e8783262550
chenp <chenp@zdlz.cc>
2025-12-04 14:44:55 +0800

    改下丑丑的main

--- main.go
+++ main.go
@@ -31,88 +31,6 @@ var (
 )
 
 func main() {
-	initializeApplication()
-	startApplication()
-}
-
-// startApplication 启动应用程序服务
-func startApplication() {
-	router = controllers.DefineRouter()
-
-	// 启动定时任务
-	setupPeriodicTasks()
-
-	fmt.Println("Welcome to bmtdblog!")
-
-	// 设置优雅关闭
-	setupGracefulShutdown()
-
-	// 创建并启动服务器
-	cfg := system.GetConfiguration()
-	srv = &http.Server{
-		Addr:    cfg.Addr,
-		Handler: router,
-	}
-
-	// 启动服务器
-	if err := system.StartServer(srv); err != nil && err != http.ErrServerClosed {
-		system.Logger.Error("Server error", "err", err)
-	}
-
-	// 清理资源
-	defer cleanupResources()
-}
-
-// setupPeriodicTasks 设置定时任务
-func setupPeriodicTasks() {
-	gocron.Every(1).Day().Do(controllers.CreateXMLSitemap)
-	gocron.Every(7).Days().Do(controllers.Backup)
-	gocron.Start()
-}
-
-// setupGracefulShutdown 设置优雅关闭
-func setupGracefulShutdown() {
-	signal.Notify(c, os.Interrupt, syscall.SIGTERM, syscall.SIGINT)
-	go clean()
-}
-
-// cleanupResources 清理资源
-func cleanupResources() {
-	if f != nil {
-		f.Close()
-	}
-	if db != nil {
-		if database, err := db.DB(); err == nil {
-			database.Close()
-		}
-	}
-}
-
-// canclue the program gracefully
-func clean() {
-	<-c
-	fmt.Println("Cleaning...")
-
-	// 停止ES任务队列
-	models.StopESTaskQueue()
-
-	// 停止邮件队列
-	system.StopEmailQueue()
-
-	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
-	if err := srv.Shutdown(ctx); err != nil {
-		system.Logger.Error("HTTP server shutdown error", "err", err)
-	}
-	gocron.Clear()
-	dbInstance, _ := db.DB()
-	_ = dbInstance.Close()
-	f.Close()
-	cancel()
-	os.Exit(0)
-}
-
-// initializeApplication 初始化应用程序
-func initializeApplication() {
 	// system.logger
 	logDir := "slog"
 	if err := os.MkdirAll(logDir, 0755); err != nil {
@@ -123,6 +41,7 @@ func initializeApplication() {
 	if err != nil {
 		panic(err)
 	}
+
 	// 调试用设置
 	var opts *slog.HandlerOptions = nil
 	opts = &slog.HandlerOptions{
@@ -175,5 +94,75 @@ func initializeApplication() {
 	} else {
 		// 设置邮件发送回调函数
 		system.SetEmailSender(helpers.SendMail)
+	}
+
+	router = controllers.DefineRouter()
+
+	// 启动定时任务
+	setupPeriodicTasks()
+
+	fmt.Println("Welcome to bmtdblog!")
+
+	// 设置优雅关闭
+	setupGracefulShutdown()
+
+	// 创建并启动服务器
+	cfg := system.GetConfiguration()
+	srv = &http.Server{
+		Addr:    cfg.Addr,
+		Handler: router,
+	}
+
+	// 启动服务器
+	if err := system.StartServer(srv); err != nil && err != http.ErrServerClosed {
+		system.Logger.Error("Server error", "err", err)
+	}
+
+	// 清理资源
+	defer cleanupResources()
+}
+
+// setupPeriodicTasks 设置定时任务
+func setupPeriodicTasks() {
+	gocron.Every(1).Day().Do(controllers.CreateXMLSitemap)
+	gocron.Every(7).Days().Do(controllers.Backup)
+	gocron.Start()
+}
+
+// setupGracefulShutdown 设置优雅关闭
+func setupGracefulShutdown() {
+	signal.Notify(c, os.Interrupt, syscall.SIGTERM, syscall.SIGINT)
+	go func() {
+		<-c
+		fmt.Println("Cleaning...")
+
+		// 停止ES任务队列
+		models.StopESTaskQueue()
+
+		// 停止邮件队列
+		system.StopEmailQueue()
+
+		ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+		if err := srv.Shutdown(ctx); err != nil {
+			system.Logger.Error("HTTP server shutdown error", "err", err)
+		}
+		gocron.Clear()
+		dbInstance, _ := db.DB()
+		_ = dbInstance.Close()
+		f.Close()
+		cancel()
+		os.Exit(0)
+	}()
+}
+
+// cleanupResources 清理资源
+func cleanupResources() {
+	if f != nil {
+		f.Close()
+	}
+	if db != nil {
+		if database, err := db.DB(); err == nil {
+			database.Close()
+		}
 	}
 }
